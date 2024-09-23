ec4bc65461eee93f2215d3b507c02b17b868e368
Daniel <danielfdoficial@gmail.com>
2024-09-23 00:12:32 -0300

    feat: register routes

--- src/modules/index.routes.ts
+++ src/modules/index.routes.ts
@@ -5,6 +5,7 @@ import AdminRoutes from './admin/admin.routes';
 import AuthRoutes from './auth/auth.routes';
 import CampaignRoutes from './campaign/campaign.routes';
 import CategoryRoutes from './category/category.routes';
+import RegisterRoutes from './register/register.routes';
 import UploadFileRoutes from './upload-file/upload-file.routes';
 
 const router = Router();
@@ -14,6 +15,7 @@ router.use('/admins', AdminRoutes);
 router.use('/auth', AuthRoutes);
 router.use('/campaigns', CampaignRoutes);
 router.use('/categories', CategoryRoutes);
+router.use('/registers', RegisterRoutes);
 router.use('/upload-file', UploadFileRoutes);
 
 export default router;
